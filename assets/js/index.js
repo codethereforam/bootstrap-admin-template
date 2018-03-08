06c767c3979a938b45b7e216f79b64574e58eb80
codethereforam <codethereforam@gmail.com>
2018-03-08 23:59:24 +0800

    动态创建iframe

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,19 @@ $(document).ready(function () {
         sidebarBodyLi.removeClass("active");
     });
 
+    // 动态创建iframe
+    var mainContent = $("#mainContent");
+    var contentInMainContent = '';
+    $(".nav-sidebar-body .nav li a").each(function () {
+        var val = $(this).attr("href").substr(1);
+        contentInMainContent += '<div id="' + val + '" class="tab-pane fade">\n' +
+            '                <iframe class="main-iframe" src="' + val + '.html" width="100%" frameborder="0"' +
+            ' scrolling="auto"></iframe>\n' +
+            '            </div>';
+    });
+    mainContent.html(contentInMainContent);
+    mainContent.find("div").first().addClass("active in");
+
     // iframe高度自适应
     $(".main-iframe").on("load", function () {
         $(this).height($(window).height() - 55);
